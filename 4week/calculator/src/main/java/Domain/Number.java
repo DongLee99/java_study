384d20e70480f0ab3f5868ba343cbc91ec1bbbf8
DongLee99 <jhnj741@naver.com>
2021-02-02 14:53:05 +0900

    fix:수정

--- 4week/calculator/src/main/java/Domain/Number.java
+++ 4week/calculator/src/main/java/Domain/Number.java
@@ -13,7 +13,7 @@ public class Number {
 
     private void validateNumber() {
         if(this.value == new BigDecimal(0) ){
-            throw new IllegalArgumentException("잘못된 값이 들어왔습니다. ");
+            throw new IllegalArgumentException("잘못된 값이 들어왔습니다.");
         }
     }
 
